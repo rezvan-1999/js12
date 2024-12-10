25f04e3130aa4860279951a7bdf115722db3c2f0
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-10 07:27:14 -0800

    the answer of q1

--- index.js
+++ index.js
@@ -6,3 +6,21 @@
  * Signals
  */
 // !Answer:
+
+function firstFunc() {
+  let word1 = "Action";
+
+  function secondFunc() {
+    let word2 = "Potential";
+
+    function thirdFunc() {
+      let word3 = "Signals";
+      console.log(word1);
+      console.log(word2);
+      console.log(word3);
+    }
+    thirdFunc();
+  }
+  secondFunc();
+}
+firstFunc();
