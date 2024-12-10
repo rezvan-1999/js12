00496d662ac46a759447956ac650a6cf9a0339ad
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-10 09:14:03 -0800

    the answer of q4

--- index.js
+++ index.js
@@ -3,3 +3,21 @@
 // todo:arrow function
 // todo:template string instead of "" and ''
 // todo:destructuring
+
+var look = "I'm a bad var!";
+
+const insteadOfVar = () => {
+  console.log(
+    `in this example I use const, arrow function and back tick. var is so bad! because now I can access to it even it isn't in this scope, look: ${look}`
+  );
+};
+insteadOfVar();
+
+// === //
+
+let destructuring = ["bahar", "tabestan", "paeez", "zemestan"];
+
+const [first, second, third, forth] = destructuring;
+console.log(
+  `the best season is: ${third} :), anddddddd I use let and destructuring in this exa too.`
+);
