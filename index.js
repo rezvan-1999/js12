648cd8e48f868bda6394188eead95c2f64a68fe6
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-10 01:33:00 -0800

    make file

---
+++ index.js
@@ -0,0 +1,6 @@
+// ? Question-2:Using higher-order functions:
+// todo-1:write two functiones(innerFunc,outerFunc).
+// todo-2:the innerFunc calculates the multiplication of two numbers.
+// todo-3:call the innerFunc in the outerFunc.
+
+// !Answer:
