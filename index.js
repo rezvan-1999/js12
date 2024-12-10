7126215368068b3b7f089bf156f84063d0eb8333
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-10 08:25:42 -0800

    the answer of q3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // todo-2: In The function that you created in todo-1, you should create two functions.
 // todo-3: the first function displays the first half of the array, and the second function displays the second half of the array..
 
-// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 /*=> output
    [ 1, 2, 3, 4, 5 ]
@@ -11,3 +11,21 @@
  */
 
 // !Answer:
+
+function getData(arr) {
+  const half = arr.length / 2;
+  function firstHalf() {
+    const firstDisplay = arr.slice(0, half);
+    console.log(firstDisplay);
+  }
+
+  function secondHalf() {
+    const secondDisplay = arr.slice(half);
+    console.log(secondDisplay);
+  }
+
+  firstHalf();
+  secondHalf();
+}
+
+getData(numbers);
